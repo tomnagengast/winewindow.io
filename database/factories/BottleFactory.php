3537731ef09df78ce2d6f2bb9a5f1b9e6df1998f
Tom Nagengast <tnagengast@gmail.com>
2021-09-13 14:40:21 -0700

    Add Bottle resources and test ideas to README

---
+++ database/factories/BottleFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Bottle;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class BottleFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Bottle::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
