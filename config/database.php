ddcffc85ce19a8d0ce2802be915d8c07aba539d4
Tom Nagengast <tnagengast@gmail.com>
2021-11-05 14:41:31 -0700

    Fix cert formatting

--- config/database.php
+++ config/database.php
@@ -104,7 +104,7 @@ return [
             'sslmode' => 'prefer',
             'sslcert' => env('DB_SSLCERT', 'client.crt'),
             'sslkey' => env('DB_SSLKEY', 'client.key'),
-            'sslrootcert' => preg_replace("/\\n/g", "\n", env('DB_SSLROOTCERT', 'ca.crt')),
+            'sslrootcert' => str_replace('\n', "\n", env('DB_SSLROOTCERT', 'ca.crt')),
         ],
 
     ],
