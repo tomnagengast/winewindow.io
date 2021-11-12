8bad91e90492a23a847b69399e9d684bb76cb7da
Tom Nagengast <tnagengast@gmail.com>
2021-11-11 17:37:03 -0800

    Update landing hero copy

--- resources/js/Pages/Welcome.vue
+++ resources/js/Pages/Welcome.vue
@@ -23,9 +23,8 @@
         </div>
         <jet-application-mark />
         <h1 class="text-4xl sm:text-6xl lg:text-7xl leading-none font-extrabold tracking-tight text-gray-800 mt-10 mb-8 sm:mt-14 sm:mb-10">
-            Connecting Wineries and Winos.</h1>
-        <p class="max-w-screen-lg text-lg sm:text-2xl sm:leading-10 font-medium mb-10 sm:mb-11">Saddle up</p>
-        <p class="max-w-screen-lg text-lg sm:text-2xl sm:leading-10 font-medium mb-10 sm:mb-11">🤠</p>
+            Connecting Wineries with Winos</h1>
+        <p class="max-w-screen-lg text-lg sm:text-2xl sm:leading-10 font-medium mb-10 sm:mb-11">Saddle up 🤠</p>
         <div class="flex flex-wrap space-y-4 sm:space-y-0 sm:space-x-4 text-center"><a
             class="w-full sm:w-auto flex-none bg-gray-800 hover:bg-gray-700 text-white text-lg leading-6 font-semibold py-3 px-6 border border-transparent rounded-xl focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-gray-900 focus:outline-none transition-colors duration-200"
             href="/dashboard">Hop into your cellar</a></div>
