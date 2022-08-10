b2fb8e345219417262ed2c4b4cc00c99179d0f8b
Tom Nagengast <tnagengast@gmail.com>
2022-08-10 12:05:50 -0700

    Add heap tracking to production

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -15,30 +15,17 @@
     {{-- <script src="{{ mix('js/manifest.js') }}" defer></script>--}}
     {{-- <script src="{{ mix('js/vendor.js') }}" defer></script>--}}
     <script src="{{ mix('js/app.js') }}" defer></script>
+    {{--<script src="{{ mix('js/amplitude.js') }}" defer></script>--}}
+
+    @env ('production')
+        <script src="{{ mix('js/heap.js') }}" defer></script>
+    @endenv
 </head>
 <body class="font-sans antialiased bg-white">
 @inertia
 
-@env ('local')
+@env ('development')
     <script src="http://localhost:3000/browser-sync/browser-sync-client.js"></script>
 @endenv
-@env ('local')
-    <script type="text/javascript">
-        window.heap = window.heap || [], heap.load = function (e, t) {
-            window.heap.appid = e, window.heap.config = t = t || {};
-            var r = document.createElement("script");
-            r.type = "text/javascript", r.async = !0, r.src = "https://cdn.heapanalytics.com/js/heap-" + e + ".js";
-            var a = document.getElementsByTagName("script")[0];
-            a.parentNode.insertBefore(r, a);
-            for (var n = function (e) {
-                return function () {
-                    heap.push([e].concat(Array.prototype.slice.call(arguments, 0)))
-                }
-            }, p = ["addEventProperties", "addUserProperties", "clearEventProperties", "identify", "resetIdentity", "removeEventProperty", "setEventProperties", "track", "unsetEventProperty"], o = 0; o < p.length; o++) heap[p[o]] = n(p[o])
-        };
-        heap.load("2696906616");
-    </script>
-@endenv
-
 </body>
 </html>
